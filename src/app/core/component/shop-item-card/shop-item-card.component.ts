1a832b99e1391243598687ab7a0be9a298c80a02
fmotsch <francis.motschwiio.fr>
2024-08-05 16:41:59 +0200

    rdv modal init

--- src/app/core/component/shop-item-card/shop-item-card.component.ts
+++ src/app/core/component/shop-item-card/shop-item-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { environment } from 'src/environments/environment';
+import { RdvModalComponent } from '../rdv-modal/rdv-modal.component';
 
 @Component({
     selector: 'app-shop-item-card',
@@ -10,4 +12,12 @@ export class ShopItemCardComponent {
     @Input() item: any;
     imgStorageUrl: string = environment.imgStorageUrl;
     // /assets/images/ubertest2.webp
+   
+    constructor(
+        public dialog: MatDialog
+    ) {}
+
+    openDialog() {
+        this.dialog.open(RdvModalComponent);
+    }
 }
