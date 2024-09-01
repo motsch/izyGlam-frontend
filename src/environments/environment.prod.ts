be7b9fbed83f1993340a9e5fa812c2bba5b9b38b
fmotsch <francis.motschwiio.fr>
2024-09-01 22:31:14 +0200

    language management updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
     production: true,
     apiUrl: 'http://192.168.13.241:3001/api/',
     imgStorageUrl: 'https://developpeurfreelance.com/assets/izi/glam/images/',
-    allowedCountries: ['FR'],
-    comingSoonCountries: ['DE', 'ES', 'IT', 'NL', 'PT'],
-    comingSoonCountriesDate: '2026-01-01',
-    comingSoonCountries2: ['SV', 'PL', 'DA', 'FI', 'DA'],
+    allowedCountries: [''],
+    comingSoonCountries: ['FR'],
+    comingSoonCountriesDate: '2025-01-01',
+    comingSoonCountries2: ['SV', 'PL', 'DA', 'FI', 'DA', 'DE', 'ES', 'IT', 'NL', 'PT'],
     comingSoonCountries2Date: '2027-01-01',
 };
